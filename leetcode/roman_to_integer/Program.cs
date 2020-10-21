a22b66b2030de7d2a1f5a24adda67efd3b655b59
Gulzat Karimova <gulzat.karimova@gmail.com>
2020-10-21 00:17:15 -0700

    Roman To Integer conversion

---
+++ leetcode/roman_to_integer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace roman_to_integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RomanToInt("IX"));
+        }
+
+        static int RomanToInt(string s)
+        {
+            Dictionary<char, int> romanNumerals = new Dictionary<char, int>() {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+            int sum = 0;
+            char previousChar = s[0];
+            foreach (char letter in s)
+            {
+                if (romanNumerals[previousChar] < romanNumerals[letter])
+                {
+                    sum -= romanNumerals[previousChar];
+                    sum += romanNumerals[letter] - romanNumerals[previousChar];
+                }
+                else
+                {
+                    sum += romanNumerals[letter];
+                }
+                previousChar = letter;
+            }
+            return sum;
+        }
+    }
+}
