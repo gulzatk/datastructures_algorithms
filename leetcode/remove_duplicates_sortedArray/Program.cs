dee0738f671d46d4075b78c782985049b7a1eac1
Gulzat Karimova <gulzat.karimova@gmail.com>
2020-10-21 20:49:46 -0700

    Created new repo that removes duplicates from sorted array.

---
+++ leetcode/remove_duplicates_sortedArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace remove_duplicates_sortedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {1,2,2,3,3,3,4};
+            Console.WriteLine(RemoveDuplicates(nums));
+        }
+
+
+        static int RemoveDuplicates(int[] nums)
+        {
+            if(nums.Length == 0)
+            {
+                return 0;
+            }
+            int temp = 0;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] != nums[i + 1])
+                {
+                    nums[temp++] = nums[i];
+                }
+
+            }
+            nums[temp] = nums[nums.Length - 1];           
+            
+            return temp + 1;
+        }
+
+    }
+}
