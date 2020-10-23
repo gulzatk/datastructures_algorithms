33ace669e03820ab958df839e38ecb9614cfcc00
Gulzat Karimova <gulzat.karimova@gmail.com>
2020-10-22 20:43:33 -0700

    Find longest common prefix.

---
+++ leetcode/longest_common_prefix/Program.cs
@@ -0,0 +1,38 @@
+﻿//Write a function to find the longest common prefix string amongst an array of string
+//If there is no prefix return an empty string "".
+
+using System;
+
+namespace longest_common_prefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = {"flow", "flower", "fly"};
+            Console.WriteLine(LongestCommonPrefix(strs));
+        }
+
+         static string LongestCommonPrefix(string[] strs)
+         {
+             if(strs.Length == 0){
+                 return "";
+             }for(int i=0; i<strs[0].Length; i++)
+             {
+                 foreach(String str in strs)
+                 {
+                     if(str.Length <= i)
+                     {
+                         return str;
+                     }
+                     if(str[i] != strs[0][i])
+                     {
+                         return str.Substring(0, i);
+                     }
+                 }
+             }
+             return strs[0];
+         }
+
+    }
+}
