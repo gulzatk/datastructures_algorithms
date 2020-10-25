a30e2bdd0ce21cd7d4ea02c06a5c97ef2e496fcc
Gulzat Karimova <gulzat.karimova@gmail.com>
2020-10-24 19:58:32 -0700

    Given a 32-bit signed integer, reverse digits of an integer.

---
+++ leetcode/reverse_integer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace reverse_integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse(123));
+            Console.WriteLine(Reverse(-123));
+          
+        }
+
+        static int Reverse(int x)
+        {
+            long reverted = 0;
+            while(x != 0)
+            {
+                int reminder = x % 10;
+                reverted = reverted * 10 + reminder;
+                x = x / 10;
+            }
+            if(reverted > Int32.MaxValue || reverted < Int32.MinValue)
+            {
+                return 0;
+            }
+            return (int)reverted;
+        }
+    }
+}
