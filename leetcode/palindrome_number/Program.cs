f7eb97cd54baa227fb6daed1f4974f3ecb4ef66e
Gulzat Karimova <gulzat.karimova@gmail.com>
2020-10-30 13:20:21 -0700

    Checking is number Palindrome.

---
+++ leetcode/palindrome_number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace palindrome_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome(12321));
+        }
+
+        static bool IsPalindrome(int x)
+        {
+            int temp = x;
+            int sum = 0;
+            while(x > 0)
+            {
+            int reminder = x % 10;
+            sum = sum * 10 + reminder;
+            x = x / 10;
+            }
+
+            if(temp == sum)
+            {
+                return true;
+            }
+            return true;
+        }
+    }
+}
