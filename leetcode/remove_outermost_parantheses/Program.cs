2d3ec846b21f0911be06bc3f874fe1dbf5ef9c5e
Gulzat Karimova <gulzat.karimova@gmail.com>
2020-11-04 10:00:47 -0800

    Remove outermost parantheses.

---
+++ leetcode/remove_outermost_parantheses/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace remove_outermost_parantheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = "(()())(())";
+            Console.WriteLine(RemoveOuterParentheses(S));
+        }
+
+        static string RemoveOuterParentheses(string S)
+        {
+            string result = "";
+            string starter = "";
+            int count = 0;
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] == '(')
+                {
+                    count++;
+                    if (count > 1)
+                    {
+                        starter += S[i];
+                    }
+                }
+                else
+                {
+                    count--;
+                    if (count > 0)
+                    {
+                        starter += S[i];
+                    }
+                    else
+                    {
+                        result += starter;
+                        starter = "";
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
