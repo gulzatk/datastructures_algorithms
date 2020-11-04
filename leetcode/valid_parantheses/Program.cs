91fad2107284307f2691caf32fc2d76d515e2592
Gulzat Karimova <gulzat.karimova@gmail.com>
2020-11-03 16:18:31 -0800

    valid parantheses with array.

---
+++ leetcode/valid_parantheses/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace valid_parantheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "{{((]))";
+            Console.WriteLine(IsValid(s));
+        }
+
+       static bool IsValid(string s)
+        {
+            List<char> p = new List<char>();
+            foreach (char ch in s)
+            {
+                if (ch == '(' || ch == '{' || ch == '[')
+                {
+                    p.Add(ch);
+                }
+                else
+                {
+                    if (p.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    char reverse;
+                    if (ch == ')')
+                    {
+                        reverse = '(';
+                    }
+                    else if (ch == '}')
+                    {
+                        reverse = '{';
+                    }
+                    else
+                    {
+                        reverse = '[';
+                    }
+                    int lastIndex = p.Count - 1;
+                    if (reverse == p[lastIndex])
+                    {
+                        p.RemoveAt(lastIndex);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return p.Count == 0;
+        }
+
+
+    }
+}
+
