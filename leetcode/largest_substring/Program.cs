d0855332d41fea3b26be85d31afbf6726b1b0a7e
Gulzat Karimova <gulzat.karimova@gmail.com>
2020-11-10 11:55:40 -0800

    Find largest substring between two equal characters.

---
+++ leetcode/largest_substring/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace largest_substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxLengthBetweenEqualCharacters("absdafhf"));
+        }
+        static int MaxLengthBetweenEqualCharacters(string s) {
+
+        }
+    }
+}
