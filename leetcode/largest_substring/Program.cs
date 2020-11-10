3fde6b03e65bf29dd3b785bd2a96ce9d494e35f6
Gulzat Karimova <gulzat.karimova@gmail.com>
2020-11-10 12:33:42 -0800

    Add comments and discription to the code.

--- leetcode/largest_substring/Program.cs
+++ leetcode/largest_substring/Program.cs
@@ -8,13 +8,15 @@ namespace largest_substring
         {
             Console.WriteLine(MaxLengthBetweenEqualCharacters("adsabvb"));
         }
+        // Given a string s, return the length of the longest substring between two equal characters, 
+        // excluding the two characters. If there is no such substring return -1.
         static int MaxLengthBetweenEqualCharacters(string s) {
             int maxLen = -1;
             
             for (int i=0; i<s.Length-1; i++){
                 for (int j=i+1; j<s.Length; j++){
                     if(s[i] == s[j]){
-                        int count = j - i - 1;
+                        int count = j - i - 1; // get distance count between i to j
                         if(count > maxLen){
                             maxLen = count;
                         }
