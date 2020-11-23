25b5ad689b202b5f53a7633bda258a6e7068ec82
Gulzat Karimova <gulzat.karimova@gmail.com>
2020-11-23 13:15:13 -0800

    Running sum of an array.

---
+++ leetcode/running_sum_of_array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace running_sum_of_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {1,2,3,4};
+            int[] result = RunningSum(nums);
+            foreach (int n in result)
+            {
+                Console.WriteLine(n);
+            }
+        }
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+// Return the running sum of nums.
+
+        static int[] RunningSum(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] = nums[i] + nums[i - 1];
+            }
+            return nums;
+        }
+    }
+}
