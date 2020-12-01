4a0d008bc0b39e49af5b15e5651f7de5fe6622d0
Gulzat Karimova <gulzat.karimova@gmail.com>
2020-12-01 14:40:32 -0800

    happy number.

---
+++ leetcode/happy_number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace happy_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             // Console.WriteLine(IsHappy(2));
+             Console.WriteLine(IsHappy(19));
+           //  Console.WriteLine(IsHappy(100));
+        }
+        static bool IsHappy(int n)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            while (n != 1)
+            {
+                if (dic.ContainsKey(n))
+                {
+                    return false;
+                }
+                int sum = 0;
+                while (n != 0)
+                {
+                    int remainder = n % 10;
+                    sum = sum + (remainder * remainder);
+                    n = n / 10;
+                }
+                dic[n] = sum;
+                n = sum;
+            }
+            return true;
+        }
+    }
+}
