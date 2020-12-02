b044d1c72eb845bd4d0bde71d863d93272042996
Gulzat Karimova <gulzat.karimova@gmail.com>
2020-12-02 11:45:26 -0800

    Length of last word.

---
+++ leetcode/length_of_last_word/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace length_of_last_word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LengthOfLastWord("Hello World!"));
+            Console.WriteLine(LengthOfLastWord("Hello "));
+            Console.WriteLine(LengthOfLastWord(" "));
+        }
+        static int LengthOfLastWord(string s)
+        {
+            if (s.Length == 0)
+            {
+                return 0;
+            }
+            s = s.TrimEnd();
+            int lastindex = s.Length-1;
+            int count = 0;
+            while (lastindex >= 0 && s[lastindex] != ' ')
+            {
+                count++;
+                lastindex--;
+            }
+            return count;
+        }
+    }
+}
