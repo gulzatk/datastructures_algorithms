3f9bcf802f20969e558201b21ca4f7ad180477f3
Gulzat Karimova <gulzat.karimova@gmail.com>
2020-12-03 12:24:01 -0800

    Increment one to the integer.

---
+++ leetcode/plus_one/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace plus_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] digits = {1,2,3};
+            PlusOne(digits);
+            foreach (int i in digits)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+        static int[] PlusOne(int[] digits)
+        {
+            int last = digits.Length-1;
+            int carryOver = 0;
+            while (last >= 0)
+            {
+                carryOver = (digits[last] + 1) / 10;
+                digits[last] = (digits[last] + 1) % 10;
+                if (carryOver == 0)
+                {
+                    break;
+                }
+                last--;
+            }
+            if (carryOver > 0)
+            {
+                int[] newDigits = new int[digits.Length+1];
+                newDigits[0] = carryOver;
+                for (int i = 1; i < newDigits.Length; i++)
+                {
+                    newDigits[i] = digits[i-1];
+                }
+                return newDigits;
+            }
+            return digits;
+        }
+    }
+}
