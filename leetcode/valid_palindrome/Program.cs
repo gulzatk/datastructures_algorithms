ea3ee89efe4f5cf08f1a1d155e1307020290abf8
Gulzat Karimova <gulzat.karimova@gmail.com>
2020-12-04 11:04:37 -0800

    Given a string, determine if its a palindrome.

---
+++ leetcode/valid_palindrome/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace valid_palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "A man, a plan, a canal: Panama";
+            Console.WriteLine(IsPalindrome(s));
+        }
+        static bool IsPalindrome(string s)
+        {
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (Char.IsLetterOrDigit(s[i]))
+                {
+                    str.Append(s[i]);
+                }
+            }
+            s = str.ToString().ToLower();
+            int j = 0; 
+            int k = s.Length-1;
+            while (j <= k)
+            {
+                if (s[j] != s[k])
+                {
+                    return false;
+                }
+                j++; 
+                k--;
+            }
+            return true;
+
+        }
+    }
+}
