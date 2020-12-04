eb15a8e700ae66ada15280b31ccecf8146d15d8c
Gulzat Karimova <gulzat.karimova@gmail.com>
2020-12-04 11:50:20 -0800

    Given a non-negative integer, repeatedly add all digits untill the result has only one digit.

---
+++ leetcode/add_digits/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace add_digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddDigits(38));
+            Console.WriteLine(AddDigits(382));
+        }
+        // Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+        static int AddDigits(int num)
+        {
+            while ((num / 10) != 0)
+            {
+                int remainder = num % 10;
+                num = (num / 10) + remainder;
+            }
+            return num;
+        }
+    }
+}
