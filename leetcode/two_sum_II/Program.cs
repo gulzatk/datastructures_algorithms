26afa3d9e186de051aa577b0a494b3cad5fb9eba
Gulzat Karimova <gulzat.karimova@gmail.com>
2020-12-07 12:07:23 -0800

    Find two numbers such that they add up to a specifi target number.

---
+++ leetcode/two_sum_II/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace two_sum_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {2,3,4};
+            int target = 6;
+            TwoSum(nums, target);
+            foreach(int i in nums)
+            {
+                Console.WriteLine(i);
+            } 
+            
+        }
+        static int[] TwoSum(int[] nums, int target)
+        {
+            int i = 0;
+            int j = nums.Length-1;
+            while (i <= j)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new int[]{i+1, j+1};
+                }
+                if (nums[i] + nums[j] < target)
+                {
+                    i++;
+                } else {
+                    j--;
+                }
+            }
+            return new int[]{i+1, j+1};
+        }
+    }
+}
