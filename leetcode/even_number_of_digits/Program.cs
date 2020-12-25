378cb0b05f47f2cf27c6c02c89c8a67f5403a28f
Gulzat Karimova <gulzat.karimova@gmail.com>
2020-12-25 15:32:47 -0800

    Find numbers with even numbers of digits.

---
+++ leetcode/even_number_of_digits/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace even_number_of_digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {12, 123, 2, 1234};
+
+            Console.WriteLine(FindNumbers(nums));
+        }
+        static int FindNumbers(int[] nums)
+        {
+            int evenDigits = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int count = 0;
+                while (nums[i] != 0)
+                {
+                    nums[i] = nums[i] / 10;
+                    count++;
+                }
+                if (count % 2 == 0)
+                {
+                    evenDigits++;
+                }
+            }
+            return evenDigits;
+        }
+    }
+}
