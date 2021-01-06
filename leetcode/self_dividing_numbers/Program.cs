4d2a96e363d25cb41d9d9c5693ead1c8d97e9724
Gulzat Karimova <gulzat.karimova@gmail.com>
2021-01-05 23:32:18 -0800

    Find self dividing numbers.

---
+++ leetcode/self_dividing_numbers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace self_dividing_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<int> result = SelfDividingNumbers(1, 15);
+            foreach (int n in result)
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        static IList<int> SelfDividingNumbers(int left, int right)
+        {
+            List<int> result = new List<int>();
+            for (int i = left; i <= right; i++)
+            {
+                if (isSelfDividing(i))
+                {
+                    result.Add(i);
+                }
+            }
+            return result;
+        }
+        static bool isSelfDividing(int number)
+        {
+            int n = number;
+            while (n > 0)
+            {
+                int digit = n % 10;
+                if (digit == 0 || number % digit != 0)
+                {
+                    return false;
+                }
+                n = n / 10;
+            }
+            return true;
+        }
+    }
+}
