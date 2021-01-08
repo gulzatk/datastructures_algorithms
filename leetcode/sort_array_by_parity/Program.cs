b1c1340aae799c750050cd3c05df39009141fb57
Gulzat Karimova <gulzat.karimova@gmail.com>
2021-01-08 13:42:13 -0800

    Sort array by parity.

---
+++ leetcode/sort_array_by_parity/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sort_array_by_parity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 3, 1, 2, 4 };
+            int[] result = SortArrayByParity(A);
+            foreach (int n in result)
+            {
+                Console.WriteLine(n);
+            }
+
+        }
+        static int[] SortArrayByParity(int[] A)
+        {
+            List<int> result = new List<int>();
+
+            foreach (int n in A)
+            {
+                if (n % 2 == 0)
+                {
+                    result.Insert(0, n);
+                }
+                else
+                {
+                    result.Add(n);
+                }
+            }
+            return result.ToArray();
+        }
+    }
+}
