dd43bab33a3143b7cf0bbbded39e82c7343ee960
Gulzat Karimova <gulzat.karimova@gmail.com>
2021-01-13 14:59:29 -0800

    Check is parantheses is valid using stack.

---
+++ leetcode/valid_parantheses_stack/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace valid_parantheses_stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsValid("({})"));
+            Console.WriteLine(IsValid("({[)})"));
+            Console.WriteLine(IsValid("({()})"));
+            Console.WriteLine(IsValid("()"));
+        }
+        static bool IsValid(string s)
+        {
+            Stack<char> myStack = new Stack<char>();
+            foreach (char ch in s)
+            {
+                if (ch == '(' || ch == '{' || ch == '[')
+                {
+                    myStack.Push(ch);
+                } else
+                {
+                    if (myStack.Count == 0)
+                    return false;
+                
+                    char reverse;
+                    if (ch == ')')
+                    {
+                        reverse = '(';
+                    }
+                    else if (ch =='}')
+                    {
+                        reverse = '{';
+                    } 
+                    else
+                    {
+                        reverse = '{';
+                    }
+                    if (reverse != myStack.Pop())
+                    {
+                        return false;
+                    }
+                }
+            }
+            return myStack.Count == 0;
+        }
+    }
+}
