cd91d12b82d72f5b2fd040350a42d4c102fa2143
Gulzat Karimova <gulzat.karimova@gmail.com>
2021-01-20 12:01:36 -0800

    Find most common word from paragraph.

---
+++ leetcode/most_common_word/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace most_common_word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+            string[] banned = {"hit"};
+            Console.WriteLine(MostCommonWord(paragraph, banned));
+        }
+        static string MostCommonWord(string paragraph, string[] banned) {
+            HashSet<string> mySet = new HashSet<string>(banned);
+            string[] words = paragraph.Replace(",", " ").Replace(".", " ").Replace(";", " ").Replace("!", " ").Replace("?", " ").Replace("'", " ").ToLower().Split(" ");
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (string word in words)
+            {
+                if (mySet.Contains(word) || word =="")
+                {
+                    continue;
+                }
+                if (!dict.ContainsKey(word))
+                {
+                    dict.Add(word, 1);
+                } else{
+                    dict[word]++;
+                }
+            }
+            int count = 0;
+            string result = "";
+            foreach (KeyValuePair<string, int> pair in dict)
+            {
+                if (pair.Value > count)
+                {
+                    count = pair.Value;
+                    result = pair.Key;
+                }
+            }
+            return result;
+        }
+    }
+}
