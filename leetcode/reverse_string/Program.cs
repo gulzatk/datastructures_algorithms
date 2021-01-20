e216a30f85904c26cc4abfe495d2d055010094c7
Gulzat Karimova <gulzat.karimova@gmail.com>
2021-01-20 13:00:18 -0800

    reverse string.

---
+++ leetcode/reverse_string/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace reverse_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] s = {'h', 'e', 'l', 'l', 'o'};
+            Console.WriteLine(ReverseString(s));
+        }
+        static char[] ReverseString(char[] s)
+        {
+            int a = 0;
+            int b = s.Length-1;
+            while (a < b)
+            {
+                char temp = s[a];
+                s[a] = s[b];
+                s[b] = temp;
+                a++;
+                b--;
+            }
+            return s;
+        }
+    }
+}
