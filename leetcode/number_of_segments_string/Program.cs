ba82abee4e045061fc6ada3f2c77e9b6f4324f7d
Gulzat Karimova <gulzat.karimova@gmail.com>
2021-01-22 13:08:47 -0800

    Count number of segments in a string.

---
+++ leetcode/number_of_segments_string/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace number_of_segments_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountSegments("hello"));
+            Console.WriteLine(CountSegments("hello, world "));
+            Console.WriteLine(CountSegments("   "));
+        }
+        static int CountSegments(string s)
+        {
+            if (s == "")
+            {
+                return 0;
+            }
+            string[] str = s.Split(" ");
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == "")
+                {
+                    continue;
+                }
+                count++;
+            }
+            return count;
+        }
+    }
+}
