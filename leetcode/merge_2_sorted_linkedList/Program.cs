bc3fa40fb985157dd180f29d048393de22d45e50
Gulzat Karimova <gulzat.karimova@gmail.com>
2021-01-22 13:07:12 -0800

    using dummy head.

--- leetcode/merge_2_sorted_linkedList/Program.cs
+++ leetcode/merge_2_sorted_linkedList/Program.cs
@@ -34,25 +34,7 @@ namespace merge_2_sorted_linkedList
         }
         static ListNode MergeTwoLists(ListNode l1, ListNode l2)
         {
-            if (l1 == null)
-            {
-                return l2;
-            }
-            if (l2 == null)
-            {
-                return l1;
-            }
-            ListNode head;
-            if (l1.val < l2.val)
-            {
-                head = l1;
-                l1 = l1.next;
-            }
-            else
-            {
-                head = l2;
-                l2 = l2.next;
-            }
+            ListNode head = new ListNode();            
             ListNode current = head;
 
             while (l1 != null && l2 != null)
@@ -77,7 +59,7 @@ namespace merge_2_sorted_linkedList
             {
                 current.next = l1;
             }
-            return head;
+            return head.next;
         }
     }
 }
