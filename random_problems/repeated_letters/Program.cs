5a32e3eb8f86d1956ab65ee203cd5f6760088768
Gulzat Karimova <gulzat.karimova@gmail.com>
2021-01-26 20:18:46 -0800

    Find repeated letters.

---
+++ random_problems/repeated_letters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace repeated_letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RepeatedLetters("hello"));
+        }
+        static string RepeatedLetters(string s)
+        {
+            int[] counter = new int[26];
+            string result = "";
+            foreach (char ch in s)
+            {
+                counter[ch - 'a']++;
+                if (counter[ch -'a'] > 1)
+                {
+                    result = ch.ToString();
+                }
+            }
+            return result;
+        }
+    }
+}
