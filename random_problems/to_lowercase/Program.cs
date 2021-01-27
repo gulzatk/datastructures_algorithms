3234e2ce739cbc5f043704f7208728ede9da6b84
Gulzat Karimova <gulzat.karimova@gmail.com>
2021-01-26 20:16:30 -0800

    Convert string to lowercase not using method.

---
+++ random_problems/to_lowercase/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace to_lowercase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ToLower("HelLo World!"));
+        }
+        static string ToLower(string s)
+        {
+            StringBuilder result = new StringBuilder();
+            int difference = 'A' - 'a';
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] >= 'A' && s[i] <= 'Z')
+                {
+                    result.Append((char)(s[i] - difference));
+                } else{
+                    result.Append(s[i]);
+                }
+            }
+            return result.ToString();
+        }
+    }    
+}
