e8f45c8b7dedf1a5cf8f3b75140a64601020a8d4
Gulzat Karimova <gulzat.karimova@gmail.com>
2021-01-26 20:08:21 -0800

    Find repeated number.

---
+++ random_problems/repeated_number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace repeated_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {1,2,3,2,4};
+            Console.WriteLine(RepeatedNumber(numbers));
+        }
+        static int RepeatedNumber(int[] numbers)
+        {
+            if (numbers == null && numbers.Length == 0)
+            {
+                return 0;
+            }
+            int[] counter = new int[numbers.Length];
+
+            foreach (int n in numbers)
+            {
+                counter[n]++;
+                if (counter[n] > 1)
+                {
+                    return n;
+                }
+            }
+            return 0;
+        }
+    }
+}
