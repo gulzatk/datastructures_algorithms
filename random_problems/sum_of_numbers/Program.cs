ff69c843acc76c43a04b30492dd2179809be07ee
Gulzat Karimova <gulzat.karimova@gmail.com>
2021-01-26 20:10:25 -0800

    Sum of numbers found in the string.

---
+++ random_problems/sum_of_numbers/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace sum_of_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumOfNumbers("Jibek123 Emil 456"));
+        }
+        static int SumOfNumbers(string s)
+        {   
+            if (s == null || s == "")
+            {
+                return 0;
+            }      
+            int sum = 0;
+            string number = "";
+            foreach (char ch in s)
+            {
+                if (Char.IsDigit(ch))
+                {
+                    number += ch;
+                } else 
+                {
+                    if (number != "")
+                    {
+                        sum += int.Parse(number);
+                        number = "";
+                    }
+                }
+            }
+
+            if (number != "")
+            {
+                sum += int.Parse(number);
+            }
+            
+            return sum;
+        }
+    }
+}
+// case 1: s = null;
+// case 2: s = "";
+// case 3: s = "Jibek Emilbek";
+// case 4: s = "123 456";
+
+
