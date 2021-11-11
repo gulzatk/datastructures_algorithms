66c4f7b1698520453230be5b92c4e30d05a633dc
Gulzat Karimova <gulzat.karimova@gmail.com>
2021-11-11 15:41:08 -0800

    factorial using recursion.

---
+++ leetcode/factorial/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            int num = 5;
+            Console.WriteLine(Factorial(n));
+            Console.WriteLine(FactorialRecursive(num));
+        }
+
+// using loop
+        public static int Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            int result = n;
+            for (int i = n-1; i > 0; i--)
+            {
+                result = result * i; 
+            }
+            return result;
+        }
+
+        // using recursive
+        public static int FactorialRecursive(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * FactorialRecursive(n - 1);
+        }
+    }
+}
