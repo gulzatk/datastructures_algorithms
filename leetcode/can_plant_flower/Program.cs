54fb573026ffddd960880697df98241ca22db812
Gulzat Karimova <gulzat.karimova@gmail.com>
2021-11-11 15:38:04 -0800

    Plant flower.

---
+++ leetcode/can_plant_flower/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace can_plant_flower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] flowerbed = {1,0,0,0,1};
+            int n = 2;
+            Console.WriteLine(CanPlaceFlower(flowerbed, n));
+        }
+
+        static bool CanPlaceFlower(int[] flowerbed, int n)
+        {
+            for (int i = 0; i < flowerbed.Length; i++)
+            {
+                if ((i - 1 < 0 || flowerbed[i - 1] == 0) && flowerbed[i] == 0 && (i + 1 == flowerbed.Length || flowerbed[i + 1] == 0))
+                {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }
+            return n <= 0;
+        }
+    }
+}
