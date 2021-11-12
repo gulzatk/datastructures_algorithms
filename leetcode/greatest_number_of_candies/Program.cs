ac9e7e5549ffe44d29dbe601875f408cde96761b
Gulzat Karimova <gulzat.karimova@gmail.com>
2021-11-12 13:18:24 -0800

    Kids with greatest number of candies.

---
+++ leetcode/greatest_number_of_candies/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace greatest_number_of_candies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] candies = {2,3,5,1,3};
+            int extraCandies = 3;
+            IList<bool> result = KidsWithCandies(candies, extraCandies);
+            foreach (bool r in result)
+            {
+                Console.WriteLine(r);
+            }
+        }
+
+        static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            List<bool> result = new List<bool>();
+            int greatest = 0;
+            foreach (int n in candies)
+            {
+                if (n > greatest)
+                {
+                    greatest = n;
+                }
+            }
+            foreach (int n in candies)
+            {
+                result.Add(n + extraCandies >= greatest ? true : false);
+            }
+            return result;
+        }
+    }
+}
