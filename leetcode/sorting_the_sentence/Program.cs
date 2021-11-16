c8c21be76a40257ba7d77f39805771f904c20604
Gulzat Karimova <gulzat.karimova@gmail.com>
2021-11-15 17:27:27 -0800

    sorting the sentence.

---
+++ leetcode/sorting_the_sentence/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace sorting_the_sentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string s = "is2 sentence4 This1 a3";
+           Console.WriteLine(SortSentence(s));
+        }
+
+        static string SortSentence(string s)
+        {
+            string[] str = s.Split(' ');
+            string[] result = new string[str.Length];
+            foreach (string item in str)
+            {
+                int index = (item[item.Length-1] - '0') - 1;
+                result[index] = item.Remove(item.Length - 1, 1);
+            }
+            return string.Join(" ", result);
+        }
+    }
+}
