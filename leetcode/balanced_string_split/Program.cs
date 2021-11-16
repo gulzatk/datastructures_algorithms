0e7d3c0f047a608f620b9bd9390700e7c3332f7c
Gulzat Karimova <gulzat.karimova@gmail.com>
2021-11-15 23:27:04 -0800

    Split a string in balanced string.

---
+++ leetcode/balanced_string_split/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace balanced_string_split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "RLRRLLRLRL";
+            Console.WriteLine(BalancedStringSplit(str));
+        }
+
+        static int BalancedStringSplit(string str)
+        {
+            int count = 0, r = 0, l = 0;
+            foreach (char c in str)
+            {
+                if (c == 'R')
+                {
+                    r++;
+                }
+                else
+                {
+                    l++;
+                }
+                if (r == l)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
