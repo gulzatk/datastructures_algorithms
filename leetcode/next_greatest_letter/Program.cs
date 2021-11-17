677b02a9ae2207c43698492aa81064668be6be42
Gulzat Karimova <gulzat.karimova@gmail.com>
2021-11-17 12:16:02 -0800

    Find smallest character in the array that is larger than target.

---
+++ leetcode/next_greatest_letter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// Given a characters array letters that is sorted in non-decreasing order and a character target, return the smallest character in the array that is larger than target.
+
+namespace next_greatest_letter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] letters = {'c','f','j'};
+            char target = 'd';
+            Console.WriteLine(NextGreatestLetter(letters, target));
+        }
+
+        static char NextGreatestLetter(char[] letters, char target)
+        {
+            foreach (char n in letters)
+            {
+                if (n > target)
+                {
+                    return n;
+                }
+            }
+            return letters[0];
+        }
+    }
+}
