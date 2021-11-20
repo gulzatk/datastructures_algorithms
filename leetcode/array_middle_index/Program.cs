578a5d4ce790f5aafad47dd47cfde7ad48c6e62f
Gulzat Karimova <gulzat.karimova@gmail.com>
2021-11-19 21:49:05 -0800

    Find the middle index in array.

---
+++ leetcode/array_middle_index/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace array_middle_index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {1,-1,2,2,8,4};
+            Console.WriteLine(FindMiddleIndex(nums));
+        }
+
+        static int FindMiddleIndex(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return 0;
+            }
+            int rightSum = 0;
+            foreach (int n in nums)
+            {
+                rightSum += n;
+            }
+            int leftSum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                rightSum -= nums[i];
+                if (leftSum == rightSum)
+                {
+                    return i;
+                }
+                leftSum += nums[i];
+            }
+            return -1;
+        }
+    }
+}
