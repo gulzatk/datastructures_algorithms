2bb99609ef64af196f657d3f2212a116596a6189
Gulzat Karimova <gulzat.karimova@gmail.com>
2021-11-23 15:59:20 -0800

    Two furthest houses with different colors.

---
+++ leetcode/houses_with_diff_colors/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace houses_with_diff_colors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] colors = {1,1,1,6,1,1,1};
+            Console.WriteLine(MaxDistance(colors));
+        }
+
+        static int MaxDistance(int[] colors)
+        {
+            int a = 0, b = colors.Length-1, diff = 1;
+            while (a < b)
+            {
+                int count = (colors.Length-1) - b;
+                if (colors[a] != colors[b] && count <= Math.Abs(a - b))
+                {
+                    if (diff < Math.Abs(a - b))
+                    {
+                        diff = Math.Abs(a - b);
+                    }
+                }
+                if (colors[a] != colors[b] && count > Math.Abs(a - b))
+                {
+                    if (diff < count)
+                    {
+                        diff = count;
+                    }
+                }
+                b--;
+            }
+            return diff;
+        }
+    }
+}
