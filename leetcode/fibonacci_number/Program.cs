00096a8d0a839f51c2283b9096251bdd3aa443bf
Gulzat Karimova <gulzat.karimova@gmail.com>
2021-11-23 16:03:28 -0800

    Fibonacci number.

---
+++ leetcode/fibonacci_number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace fibonacci_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            Console.WriteLine(Fib(n));
+        }
+
+        static int Fib(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            int a = 0, b = 1, c = 1;
+            while (n > 1)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                n--;
+            }
+            return c;
+        }
+    }
+}
